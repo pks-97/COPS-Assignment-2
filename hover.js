5404f2f5374d28c72ed5d488cfd0c20339c84a81
Pratyush Sahoo <pratyushk.sahoo.min16@itbhu.ac.in>
2017-12-11 00:08:40 +0530

    add files

---
+++ hover.js
@@ -0,0 +1,48 @@
+var im=document.getElementsByTagName("img")[0];
+var im1=document.getElementsByTagName("img")[1];
+var im2=document.getElementsByTagName("img")[2];
+var im3=document.getElementsByTagName("img")[3];
+var im4=document.getElementsByTagName("img")[4];
+var im5=document.getElementsByTagName("img")[5];
+var t=document.querySelector("#PIC1");
+im.addEventListener("mouseover",function(){
+	im.setAttribute("src","https://www.barrierreef.org/generated/picture-large/layer-55-jpg.jpg?1468214114");
+})
+im.addEventListener("mouseout",function(){
+	
+	
+	im.setAttribute("src", "http://www.australia.com/content/australia/de_de/places/great-barrier-reef/_jcr_content/hero/image.adapt.1663.medium.jpg");
+})
+im1.addEventListener("mouseover",function(){
+	im1.setAttribute("src","https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2d/e6/melbourne.jpg");
+})
+im1.addEventListener("mouseout",function(){
+	im1.setAttribute("src","http://www.australia.com/content/australia/en_sg/itineraries/nsw-3weeks-sydney/_jcr_content/image.adapt.761.HIGH.jpg");
+})
+im2.addEventListener("mouseover",function(){
+	im2.setAttribute("src","https://cdn2.queensland.com/~/media/5177d6785b264dc79e6d39110cc8a3a7.ashx?as=1&la=en-us&w=565&vs=1&d=00010101T000000");
+})
+im2.addEventListener("mouseout",function(){
+	im2.setAttribute("src","https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5573538363001_5541842355001-vs.jpg?pubId=5104226627001&videoId=5541842355001");
+})
+im3.addEventListener("mouseover",function(){
+	im3.setAttribute("src","http://southaustralia.com/~/media/consumer/images/pageimages/domestic/places%20to%20go/kangaroo%20island/kangaroo-island-remarkable-rocks-view.ashx");
+})
+im3.addEventListener("mouseout",function(){
+	im3.setAttribute("src","http://www.lincolngreenfarmstay.com/uploads/2/3/5/4/23542620/efimageid-53984_orig.jpg");
+})
+im4.addEventListener("mouseover",function(){
+	im4.setAttribute("src","http://cd.visitmelbourne.com/-/media/images/melbourne/destinations/luna-park-st-kilda_mel_r_supplied_1150x863.jpg?ts=20170324010236&cp=95&w=480&h=360&crop=1|http://cd.visitmelbourne.com/-/media/images/melbourne/destinations/luna-park-st-kilda_mel_r_supplied_1150x863.jpg?ts=20170324010236&cp=95&w=720&h=540&crop=1");
+})
+im4.addEventListener("mouseout",function(){
+	im4.setAttribute("src","http://www.telegraph.co.uk/content/dam/Travel/Destinations/Oceania/Australia/Melbourne/melbourne-nightlife-skyline-xlarge.jpg");
+})
+im5.addEventListener("mouseover",function(){
+	im5.setAttribute("src","https://static1.squarespace.com/static/574bedb34d088e3b76299bc8/t/59a4afb2e45a7c96f2eebc0d/1503965111867/image.adapt.761.HIGH.jpg");
+})
+im5.addEventListener("mouseout",function(){
+	im5.setAttribute("src","http://www.australia.com/content/australia/en_sg/itineraries/vic-3days-melbourne/_jcr_content/image.adapt.761.HIGH.jpg");
+})
+
+
+
